d6e59f5024b3a4e4e021f220045f7c2742a6f8eb
miloszAlejster <milosz.alejster@gmail.com>
2022-12-05 23:08:10 +0100

    added option to change graphics

--- src/scenes/OptionsScene.ts
+++ src/scenes/OptionsScene.ts
@@ -14,29 +14,36 @@ export default class OptionsScene extends Phaser.Scene{
     immortality: Phaser.GameObjects.Text
     defaultImmortality: boolean = false
     defaultImmortalityText: string
+    // graphic
+    graphic: Phaser.GameObjects.Text
+    defaultGraphic: boolean = false
+    defaultGraphicText: string
     create(){
         // texts
-        this.p1StartHp = this.add.text(this.scale.width/2, this.scale.height/3-30, "", { fontSize: '10px'}).setOrigin(0.5)
-        this.p2StartHp = this.add.text(this.scale.width/2, 2*this.scale.height/3-30, "", { fontSize: '10px'}).setOrigin(0.5)
-        this.immortality = this.add.text(this.scale.width/2, this.scale.height-30, "", { fontSize: '10px'}).setOrigin(0.5)
+        this.p1StartHp = this.add.text(this.scale.width/2, this.scale.height/4-30, "", { fontSize: '10px'}).setOrigin(0.5)
+        this.p2StartHp = this.add.text(this.scale.width/2, 2*this.scale.height/4-30, "", { fontSize: '10px'}).setOrigin(0.5)
+        this.immortality = this.add.text(this.scale.width/2, 3*this.scale.height/4-30, "", { fontSize: '10px'}).setOrigin(0.5)
+        this.graphic = this.add.text(this.scale.width/2, this.scale.height-30, "", { fontSize: '10px'}).setOrigin(0.5)
         // inputs
         this.input.keyboard.on('keydown-UP', () => {
             if(--this.idVCursor <= -1){
-                this.idVCursor = 2
+                this.idVCursor = 3
             }
         })
         this.input.keyboard.on('keydown-DOWN', () =>{
-            if(++this.idVCursor >= 3){
+            if(++this.idVCursor >= 4){
                 this.idVCursor = 0
             }
         })
         this.input.keyboard.on('keydown-LEFT', () => {
             if(this.idVCursor === 0 && this.defaultP1StartHp > 10){
-                this.defaultP1StartHp -= 10
+                this.defaultP1StartHp -= 10;
             }else if(this.idVCursor === 1 && this.defaultP2StartHp > 10){
-                this.defaultP2StartHp -= 10
+                this.defaultP2StartHp -= 10;
             }else if(this.idVCursor === 2){
-                this.defaultImmortality = !this.defaultImmortality
+                this.defaultImmortality = !this.defaultImmortality;
+            }else if(this.idVCursor === 3){
+                this.defaultGraphic = !this.defaultGraphic;
             }
         })
         this.input.keyboard.on('keydown-RIGHT', () =>{
@@ -46,13 +53,16 @@ export default class OptionsScene extends Phaser.Scene{
                 this.defaultP2StartHp += 10
             }else if(this.idVCursor === 2){
                 this.defaultImmortality = !this.defaultImmortality
+            }else if(this.idVCursor === 3){
+                this.defaultGraphic = !this.defaultGraphic;
             }
         })
         this.input.keyboard.on('keydown-ESC', () => {
             const settings: Types.gameSettings = {
                 hp1: this.defaultP1StartHp,
                 hp2: this.defaultP2StartHp,
-                immortality: this.defaultImmortality
+                immortality: this.defaultImmortality,
+                graphic: this.defaultGraphic
             }
             this.scene.start(SceneKeys.MenuScene, settings)
         })
@@ -70,20 +80,34 @@ export default class OptionsScene extends Phaser.Scene{
             this.defaultImmortalityText = "No"
         }
         this.immortality.setText(`Should players be immortal: ${this.defaultImmortalityText}`)
+        if(this.defaultGraphic === true){
+            this.defaultGraphicText = "Graphical"
+        }else if(this.defaultGraphic === false){
+            this.defaultGraphicText = "ASCII"
+        }
+        this.graphic.setText(`Type of graphic: ${this.defaultGraphicText}`)
     }
     handleChangeCursor(){
         if(this.idVCursor == 0){
             this.p1StartHp.style.setFontSize('15px')
             this.p2StartHp.style.setFontSize('10px')
             this.immortality.style.setFontSize('10px')
+            this.graphic.style.setFontSize('10px')
         }else if(this.idVCursor === 1){
             this.p2StartHp.style.setFontSize('15px')
             this.p1StartHp.style.setFontSize('10px')
             this.immortality.style.setFontSize('10px')
+            this.graphic.style.setFontSize('10px')
         }else if(this.idVCursor === 2){
             this.immortality.style.setFontSize('15px')
             this.p1StartHp.style.setFontSize('10px')
             this.p2StartHp.style.setFontSize('10px')
+            this.graphic.style.setFontSize('10px')
+        }else if(this.idVCursor === 3){
+            this.graphic.style.setFontSize('15px')
+            this.p1StartHp.style.setFontSize('10px')
+            this.p2StartHp.style.setFontSize('10px')
+            this.immortality.style.setFontSize('10px')
         }
     }
 }
